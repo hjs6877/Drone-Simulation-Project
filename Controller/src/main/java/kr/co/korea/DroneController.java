52a0f2e1879157e12142708a73d51c200bc2fd5e
jshwang <jshwang@pentasecurity.com>
2016-03-09 17:32:44 +0900

    동적 모드 실험 중..

--- Controller/src/main/java/kr/co/korea/DroneController.java
+++ Controller/src/main/java/kr/co/korea/DroneController.java
@@ -585,7 +585,7 @@ public class DroneController {
             long flightTime = setting.getFlightTime();
 
             if(leaderOrFollower.equals("L")){
-                errorEvent = errorEventProvider.createRandomErrorEvent(flightTime, ErrorLevel.ORDINARY);
+                errorEvent = errorEventProvider.createRandomErrorEvent(flightTime, ErrorLevel.WEAK);
             }else{
                 errorEvent = errorEventProvider.createRandomErrorEvent(flightTime, ErrorLevel.WEAK);
             }
